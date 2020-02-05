db33ffbe6000f5c102c23ebe6ad5650da0e06005
karenagim <60666182+karenagim@users.noreply.github.com>
2020-02-05 10:58:13 -0300

    ejercicio 8- Sacar resto
    Usos del Simbolo % Aplicado para Modulo .

---
+++ jsEntradaSalida-8.js
@@ -0,0 +1,20 @@
+/*Debemos lograr tomar Los numeros por ID ,
+transformarlos a enteros (parseInt),realizar la operación correcta y 
+mostrar el resto entre el dividendo y el divisor.
+ej.: "El resto es 0 ."*/
+function SacarResto()
+{
+	var dividendo ;
+	var	divisor;
+	var resultadoResto;
+
+	dividendo= parseInt(document.getElementById('numeroDividendo').value) ;
+
+	divisor=   parseInt(document.getElementById('numeroDivisor').value) ;
+
+	resultadoResto  =  dividendo % divisor ;
+
+	console.log (resultadoResto );
+
+}
+ 
