2ad1d708329d1a1dc51a475c14bc10619e5624f5
karenagim <60666182+karenagim@users.noreply.github.com>
2020-02-05 12:13:17 -0300

    Ejercicio 10 + 11 Uso del parseInt / calculo de Aumento y descuento/
    Adicional ejercicio 11 Lo mismo q el 10 pero se agrega uso de PROMPT!

--- jsEntradaSalida-10.js
+++ jsEntradaSalida-10.js
@@ -4,12 +4,25 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	/*
 	var valorImporte ;
 	var resultadoSueldoConDescuento ;
 
 	valorImporte = parseInt(document.getElementById('importe').value) ;
 	resultadoSueldoConDescuento= valorImporte - (valorImporte*25/100);
 
-	console.log (resultadoSueldoConDescuento);
+	console.log (resultadoSueldoConDescuento);*/
+
+	var valorImporte ;
+	var resultadoSueldoConDescuento ;
+
+
+	valorImporte = parseInt(document.getElementById('importe').value) ;
+	resultadoSueldoConDescuento= valorImporte - (valorImporte*25/100);
+
+	prompt(" Tu importe es: " + valorImporte," Tu importe con Descuento " +resultadoSueldoConDescuento);
+
 }
+
+//Medidr dos datos por prompt y mostrar los valores del importe
 
