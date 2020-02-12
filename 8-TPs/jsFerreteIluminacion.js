2d0976037a32ce70cb647e1af6dfe8ecbd46383f
karen <karena@gmail.com>
2020-02-12 12:05:34 -0300

    Ferrete Iluminacion

--- 8-TPs/jsFerreteIluminacion.js
+++ 8-TPs/jsFerreteIluminacion.js
@@ -10,5 +10,75 @@ E.	Si el importe final con descuento suma más de $120  se debe sumar un 10% de 
  */
 function CalcularPrecio () 
 {
- 	
+ 	var cantidadLampara ;//numero
+ 	var precioLampara	;
+ 	var precioTotal;
+    var marcaLampara
+    var porcentajeDescuento
+
+ 	cantidadLampara = parseInt(document.getElementById('Cantidad').value);
+ 	precioLampara	= parseInt(35)* cantidadLampara;
+ 	marcaLampara 	= document.getElementById('Marca').value
+ 	porcentajeDescuento= 0
+	
+	//descuento		=  parseInt(10/100
+//A.	Si compra 6 o más  lamparitas bajo consumo tiene un descuento del 50%.  	
+ 	if (cantidadLampara>5)
+ 	{
+ 		porcentajeDescuento= 50
+ 		alert(precioLampara - precioLampara*porcentajeDescuento/100)
+ 	}
+//B.	Si compra 5  lamparitas bajo consumo marca "ArgentinaLuz" 
+//se hace un descuento del 40 % y si es de otra marca el descuento es del 30%.
+
+	if( cantidadLampara == 5)
+	{
+		if( marcaLampara == "ArgentinaLuz")
+		{
+			porcentajeDescuento= 40
+			alert(precioLampara - precioLampara*porcentajeDescuento/100);
+		}
+		
+		else( marcaLampara != "ArgentinaLuz")
+		{
+			porcentajeDescuento= 30
+			alert(precioLampara - precioLampara*porcentajeDescuento/100);
+		}
+	}
+	if( cantidadLampara == 4)
+	{
+		if( marcaLampara == "ArgentinaLuz"||marcaLampara == "FelipeLamparas")
+		{
+			porcentajeDescuento= 25
+			alert(precioLampara - precioLampara*porcentajeDescuento/100);
+		}
+		
+		else( !( marcaLampara == "ArgentinaLuz"||marcaLampara == "FelipeLamparas"))
+		{
+			porcentajeDescuento= 20
+			alert(precioLampara - precioLampara*porcentajeDescuento/100);
+		}
+	}
+	if( cantidadLampara == 3)
+	{
+		if( marcaLampara == "ArgentinaLuz")
+		{
+			porcentajeDescuento= 15
+			alert(precioLampara - precioLampara*porcentajeDescuento/100);
+		}
+		else
+		{
+			if( marcaLampara == "FelipeLamparas")
+			{
+				porcentajeDescuento= 10
+				alert(precioLampara - precioLampara*porcentajeDescuento/100);
+			}
+			else
+			{
+				porcentajeDescuento=5
+				alert(precioLampara - precioLampara*porcentajeDescuento/100);
+			}
+		}
+	}
+precioTotal = precioLampara - precioLampara*porcentajeDescuento/100
 }
