ddba9f7b73d7a338c521878a49353ea0f83af243
karenagim <60666182+karenagim@users.noreply.github.com>
2020-02-18 11:57:45 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,28 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
-alert (mesDelAño);
+	switch(mesDelAño)
+	{
+		case "Diciembre":
+		case "Enero":
+		case "Febrero":
+			alert("Ya pasamos el frio, ahora calor!!!.");
+		break;
 
+		case "Marzo":
+		case "Abril":
+		case "Mayo":
+			alert("Falta para el invierno.");
+		break;
 
+		default:
+			alert("Abrigate que hace frio.");
+	}
+	break;	
+
+//alert (mesDelAño);
 
 
 }//FIN DE LA FUNCIÓN
--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,10 +1,29 @@
 function mostrar()
 {
 //tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+	var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+	switch(mesDelAño)
+	{
+		case "Enero":
+		case "Diciembre":
+		case "Marzo":
+		case "Mayo":
+		case "Julio":
+		case "Agosto":
+		case "Octubre":
+
+			alert("tiene 31 dias");
+		break;
+
+		case "Febrero":
+			alert("tiene 28 dias");
+		break;
+
+		default:
+			alert("tiene 30 dias");
+	}
 	
 
 
