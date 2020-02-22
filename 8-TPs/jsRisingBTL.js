b1fc31f4fafdf60a36a112a63c27b47337aefcc9
Karen gimenez <24karengimenez@gmail.com>
2020-02-22 08:42:05 -0300

    rising btl

--- 8-TPs/jsRisingBTL.js
+++ 8-TPs/jsRisingBTL.js
@@ -9,20 +9,24 @@ F.	Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para
  */
 function ComenzarIngreso () 
 {
-	var edad;
-	var sexo;
-	var estadoCivil;
+	var edad; 			//A
+	var sexo; 			//B
+	var estadoCivil;	//C
+	var sueldoBruto		//D
+	var numeroDeLegajo	//E
+	var nacionalidad    //F
 
-	edad= prompt("ingrese una edad valida entre 18y90")
+	edad= prompt("ingrese una edad valida entre 18y90");
 	sexo=prompt("F o M para indicar el sexo");
-	estadoCivil= prompt("indique su estado Civil")
-	//estadoCivil=prompt("indique su estado Civil")
+	estadoCivil= prompt("indique su estado Civil 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos");
+	numeroDeLegajo=prompt("indique su Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.");
+	nacionalidad= prompt("Indique su Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
 
 
 
 	while( isNaN(edad)|| edad <17 || edad>100)
 	{
-		edad= prompt("ingrese una edad valida entre 18y90")
+		edad= prompt("ingrese una edad valida entre 18y90");
 	
 	}
 		document.getElementById('Edad').value = edad;
@@ -30,15 +34,34 @@ function ComenzarIngreso ()
 	
 	while( sexo != " f " && sexo != " m ")
 	{
-		sexo=prompt("f o m para indicar el sexo")
+		sexo=prompt("f o m para indicar el sexo");
 	}
 		document.getElementById('Sexo').value = sexo;
 	
-	while( estadoCivil != "casado" ||estadoCivil != "soltero" ||estadoCivil != "divorciado")
+	while( estadoCivil<1 || estadoCivil>4 || isNaN(estadoCivil) )
+	//estadoCivil != "casado" ||estadoCivil != "soltero" ||estadoCivil != "divorciado"
 	{
-		estadoCivil=prompt("indique su estado Civil")
+		estadoCivil=prompt("indique su estado Civil 1-para soltero, 2-para casados, 3-para divorciados y 4-para viudos")
 	}
  	
- 	document.getElementById('EstadoCivil').value = estadoCivil;
+ 		document.getElementById('EstadoCivil').value = estadoCivil;
+
+ 	while( Sueldo <7999)
+ 	{
+ 		sueldoBruto= prompt("Indique su sueldoBruto no menor a 8000");
+ 	}
+ 		document.getElementById('sueldo')= sueldo;
+
+ 	while()
+ 	{
+ 		numeroDeLegajo=prompt("indique su Número de legajo, numérico de 4 cifras, sin ceros a la izquierda.")
+ 	}
+
+ 	while(nacionalidad != "A" || nacionalidad != "E" || nacionalidad != "N" )
+ 	{
+ 		nacionalidad= prompt("Indique su Nacionalidad, “A” para argentinos, “E” para extranjeros, “N” para nacionalizados.");
+ 	}
+ 		document.getElementById('Nacionalidad')= nacionalidad;
 
 }
+
