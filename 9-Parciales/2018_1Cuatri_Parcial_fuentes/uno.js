092daa5a9ab0ec2c357037cf9b38d15b1fba002c
Karen gimenez <24karengimenez@gmail.com>
2020-02-25 22:30:20 -0300

    archivo parcial 1

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,18 @@
-
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+	var valorDeVerdadAncho = isNaN(ancho);
+	var valorDeVerdadLargo = isNaN(largo);
+
+	ancho= prompt("ingrese el ancho");
+	ancho= parseInt(ancho);
+
+	largo= prompt("ingrese el largo");
+	largo= parseInt(largo);
+
+	perimetro = (ancho* 2) + (largo* 2)
+	alert(perimetro);
 
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,24 @@
 
-function mostrar()
+	function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+	var valorDeVerdadAncho = isNan(ancho);
+	var valorDeVerdadLargo = isNan(largo);
+/*
+	ancho= prompt("ingrese el ancho");
+	ancho= parseInt(ancho);
 
+	largo= prompt("ingrese el largo");
+	largo= parseInt(largo);
+
+	perimetro = (ancho* 2) + (largo* 2)
+	alert(perimetro);
+
+*/
+
+alert("hola")
 }
+
+
