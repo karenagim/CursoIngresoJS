d0abe42513200924394093ce6dedbb117ab08f77
Karen gimenez <24karengimenez@gmail.com>
2020-03-05 12:09:27 -0300

    practica

--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/seis.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/seis.js
@@ -1,4 +1,42 @@
 function mostrar()
 {
+	var hora;
+	hora = document.getElementById('laHora').value;
+	hora = parseInt(hora);
+	switch(hora)
+	{
+		case 6:
+		case 7:
+		case 8:
+		case 9:
+		case 10:
+		case 11:
+		alert("es de mañana");
+		break;
 
+
+		case 12:
+		case 13:
+		case 14:
+		case 15:
+		case 16:
+		case 17:
+		case 18:
+		case 19: 
+		alert("es de tarde");
+		break;
+
+		default:
+		if(hora >20 && hora <24)
+		{
+			alert(" a dormir")
+		}
+		else
+		{
+			if(hora >24)
+			{
+				alert("la hora no es valida");
+			}
+		}
+	}
 }
--- 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2019_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,23 @@
 function mostrar()
 {
+	var alturaJugador;
+	var sexoJugador;
+
+	alturaJugador= prompt("Ingrese la altura del Jugador, hasta 250cm");
+	alturaJugador= parseInt(alturaJugador)
+
+	sexoJugador = prompt(" Ingrese el sexo del jugador f o m");
+	
+	while(isNaN(alturaJugador) && alturaJugador>0 || alturaJugador>250)
+	{
+		alturaJugador= prompt("Ingrese la altura del Jugador, hasta 250cm");
+		alturaJugador= parseInt(alturaJugador);
+	}
+	while(!isNaN(sexoJugador) || sexoJugador !="f" && sexoJugador!="m")
+	{
+		sexoJugador= prompt("Ingrese el sexo del jugador f o m");
+		sexoJugador= parseInt(sexoJugador);
+	}
+
 
 }
